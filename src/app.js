854f2d3cb3f609713a260d9565851775dd104400
Mert CC <mertfurkandgn1912@gmail.com>
2025-09-04 22:36:35 +0300

    add basic config and cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
 const app = express();
 
+// max JSON body limit
+app.use(express.json({ limit: "16kb" }))
+
+// max URL-encoded body limit
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
+// public folder static service
+app.use(express.static("public"))
+
+
 app.get('/',(req,res)=>{
-
     res.send('HELLO WORLD')
-
 });
 
 app.get("/ping", (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 
-dotenv.config();
+dotenv.config({
+  path: "./.env",
+});
 
 const port = process.env.PORT || 3000;
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
