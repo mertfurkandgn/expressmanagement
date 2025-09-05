6d10bb908ef28c142fb6c3535736af4a9678c097
Mert CC <mertfurkandgn1912@gmail.com>
2025-09-05 17:00:04 +0300

    mongodb connect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+  });
