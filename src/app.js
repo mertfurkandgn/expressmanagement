a8fc5f7fd4d56b02766ff34cff8d099275487f63
mertfurkandgn <mertfurkandgn1912@gmail.com>
2025-09-10 23:43:35 +0300

    add db model

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 
 // max JSON body limit
@@ -9,6 +10,22 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // public folder static service
 app.use(express.static("public"));
+
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//  import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get("/", (req, res) => {
   res.send("HELLO WORLD");
